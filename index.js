974e6efc96bdc446955894ad8b6cd8067d830b02
imaz-techie <mohammedimaz2264@gmail.com>
2025-05-26 17:41:44 +0530

    Overall UI completed

---
+++ index.js
@@ -0,0 +1,39 @@
+const targetDate = new Date("May 31 2025 00:00:00").getTime();
+
+function countdown() {
+  const currentDate = new Date().getTime();
+  const distance = targetDate - currentDate;
+
+  let days = Math.floor(distance / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(distance / 1000 / 60) % 24;
+  let minutes = Math.floor(distance / 1000 / 60) % 60;
+  let seconds = Math.floor(distance / 1000) % 60;
+
+  if (days < 10) {
+    days = `0${days}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  if (distance <= 0) {
+    days.innerHTML = "0";
+    hours.innerHTML = "0";
+    minutes.innerHTML = "0";
+    seconds.innerHTML = "0";
+  }
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  //   console.log(days + ":" + hours + ":" + minutes + ":" + seconds);
+}
+setInterval(countdown, 1000);
